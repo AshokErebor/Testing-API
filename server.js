77e36bc779da56ebca5fea6dc94a84497786dd5c
AshokErebor <ashok@erebortech.com>
2025-08-04 10:39:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 3000;
 app.use(requestLogger);
 app.use(express.json());
 app.use(cors());
-app.use(timeout("90s"));
+app.use(timeout("270s"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
