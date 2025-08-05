46b197b86187ce80cb23ba36e9d3139d0ef8ce8c
AshokErebor <ashok@erebortech.com>
2025-08-05 15:42:33 +0530

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { createClient } = require("redis");
 const responseModel = require("../models/ResponseModel");
 const { getContainer, getDataByQuery } = require("../services/cosmosService");
